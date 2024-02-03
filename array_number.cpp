d81eb5c9713a0a999193d17e334471a4ce472384
Akshit1711 <akshitchotaliya10@gmail.com>
2024-02-03 18:33:26 +0530

    arrary

---
+++ array_number.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+
+    // array of numbers
+//     int arr[10] = {0,1,2,3,4,5,6,7,8,9};
+//     cout << arr[9];
+//     int arr2[10];
+
+
+    
+// for (int i = 0; i < 9; i++)
+//    {
+//      cin >> arr2[i];
+//      cout <<" ";
+//    }
+// for (int j = 0; j < 9; j++)
+//    {
+//      cout << arr2[j];
+//      cout <<" "<< endl;
+//    }
+     
+
+
+
+
+// list of number and string in array
+
+// string name[10]={"aksh","aksi","raja","baja","bend","time","line","arts","oala","gola"};
+// double phone_no[10]={123,456,789,465,132,852,741,986,123,487};
+// double pincode[10]={124578,124578,124578,124544,124544,124587,124587,124578,124578,124578};
+// int index[100]={1,2,3,4,5,6,7,8,9,10};
+
+// //list of array
+
+
+// cout <<"******************************************"<<endl;
+// for (int i = 0; i < 10; i++)
+// {
+   
+//    cout <<"  *  "<<index[i]<<"  *  " << name[i] <<"  *  "<<phone_no[i]<<"  *  " <<pincode[i]<< " * "<<endl;
+// }
+// cout <<"******************************************"<<endl;
+
+
+
+
+
+
+
+
+
+
+// int count;
+// cout<<"enter number of members :"<<endl;
+// cin >> count;
+//  string name[count];
+// for (int i = 0; i < count; i++) {
+//         cout << "Enter member " << i + 1 << " full name: " << endl;
+//         cin.ignore(); // Ignore any leftover newline characters
+//         getline(cin, name[i]); // Use getline to handle names with spaces
+//     }
+
+//     cout << "Entered names:" << endl;
+//     for (int i = 0; i < count; i++) {
+//         cout << name[i] << endl;
+//     }
+    
+
+
+
+    
+
+  
+}
+
+
+
+
+
+
+
+
+
+
